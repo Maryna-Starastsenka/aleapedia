a22a0db72a8c816aeb9d21fa59036f2f21cf8b64
Maryna Sirotsina <maryna.sirotsina@gmail.com>
2019-11-02 17:02:10 -0400

    Programmation 1 - TP1 : énoncé

---
+++ Programmation1/tp1/js/tp1.js
@@ -0,0 +1,34 @@
+/* Auteurs: PERSONNE 1 (matricule), PERSONNE 2 (matricule)
+
+   TODO : Remplacez ce commentaire par un commentaire expliquant
+   l'utilité de ce programme
+ */
+
+function noirEtBlanc(imageOriginale) {
+    console.log('TODO'); // TODO : Compléter cette fonction
+    return imageOriginale; // Remplacer par la nouvelle image
+}
+
+function correctionClarte(imageOriginale, quantite) {
+    console.log('TODO'); // TODO : Compléter cette fonction
+    return imageOriginale; // Remplacer par la nouvelle image
+}
+
+function flou(imageOriginale, taille) {
+    console.log('TODO'); // TODO : Compléter cette fonction
+    return imageOriginale; // Remplacer par la nouvelle image
+}
+
+function detectionContours(imageOriginale) {
+    console.log('TODO'); // TODO : Compléter cette fonction
+    return imageOriginale; // Remplacer par la nouvelle image
+}
+
+// ==> N'hésitez pas à ajouter vos propres fonctions pour vous aider <==
+// function ...
+
+function tests() {
+    /* TODO : Ajoutez des tests unitaires pour les 4 fonctions
+       demandées et pour vos propres fonctions */
+}
+
